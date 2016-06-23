489eddfad4ac00eca37644ae34d84b578b981ed4
VS <VIVEKLUCKY1848@users.noreply.github.com>
2016-06-23 23:24:39 +0530

    Update listFilesInFolders.bat

--- listFilesInFolders.bat
+++ listFilesInFolders.bat
@@ -1,17 +1,17 @@
-Make a list of files in the current directory recursively with their full paths
+REM Make a list of files in the current directory recursively with their full paths
 dir /b /s /a:-D > <filename>.txt
 dir /b /s /a:-D > modfiles.txt
 
-Make a list of files of current folder recursively with their file sizes
+REM Make a list of files of current folder recursively with their file sizes
 @echo off & for /f %a in ('dir /s /b') do echo %~fa %~za
 Or
 @echo Results on %DATE% for %CD% >> YourDirFile.txt & echo off & for /f "delims=*" %A in ('dir /s /b /a:-d') do echo %~fA %~zA >> modfiles.txt
 @echo off & for /f %A in ('dir /s /b /a:-d') do echo %~zA %~fA %~zA
 @echo off & for /f "delims=*" %A in ('dir /s /b /a:-d') do echo %~fA %~zA
-Or
+REM Or
 FORFILES /S /M *.doc /C "cmd /c echo @fsize"
 FORFILES /M *.txt /C "cmd /c if @isdir==FALSE notepad.exe @file"
-Or
+REM Or
 forfiles /s /c "cmd /c if @isdir==FALSE echo @path @fsize" >> <filename>.txt
 forfiles /s /c "cmd /c if @isdir==FALSE echo @path @fsize" >> filesList.txt
 forfiles /s  /c "cmd /c if @isdir==FALSE echo @path @fsize"
