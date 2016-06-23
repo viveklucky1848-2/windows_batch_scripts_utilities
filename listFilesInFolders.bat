f01b3e0a88f1bc5154f3595670e178ee98300a50
VS <VIVEKLUCKY1848@users.noreply.github.com>
2016-06-23 23:23:40 +0530

    Create listFilesInFolders.bat

---
+++ listFilesInFolders.bat
@@ -0,0 +1,32 @@
+Make a list of files in the current directory recursively with their full paths
+dir /b /s /a:-D > <filename>.txt
+dir /b /s /a:-D > modfiles.txt
+
+Make a list of files of current folder recursively with their file sizes
+@echo off & for /f %a in ('dir /s /b') do echo %~fa %~za
+Or
+@echo Results on %DATE% for %CD% >> YourDirFile.txt & echo off & for /f "delims=*" %A in ('dir /s /b /a:-d') do echo %~fA %~zA >> modfiles.txt
+@echo off & for /f %A in ('dir /s /b /a:-d') do echo %~zA %~fA %~zA
+@echo off & for /f "delims=*" %A in ('dir /s /b /a:-d') do echo %~fA %~zA
+Or
+FORFILES /S /M *.doc /C "cmd /c echo @fsize"
+FORFILES /M *.txt /C "cmd /c if @isdir==FALSE notepad.exe @file"
+Or
+forfiles /s /c "cmd /c if @isdir==FALSE echo @path @fsize" >> <filename>.txt
+forfiles /s /c "cmd /c if @isdir==FALSE echo @path @fsize" >> filesList.txt
+forfiles /s  /c "cmd /c if @isdir==FALSE echo @path @fsize"
+for /r %i in (*) do @echo %~ni
+for /r %i in (*) do @echo %~nxi
+dir /s /b *.mp3
+dir /s /b *.php
+
+setlocal EnableDelayedExpansion
+for /L %%n in (1 1 500) do if "!__cd__:~%%n,1!" neq "" set /a "len=%%n+1"
+setlocal DisableDelayedExpansion
+for /r . %%g in (*.log) do (
+  set "absPath=%%g"
+  setlocal EnableDelayedExpansion
+  set "relPath=!absPath:~%len%!"
+  echo(!relPath!
+  endlocal
+)
